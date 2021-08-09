ab6bd0f3d82c585f3dd055054d8f441ad1b84e36
milan.zivanov <milan.zivanov@gmail.com>
2021-08-09 13:52:42 +0200

    Gather data from GitHub using Axios

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import axios from "axios";
+
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const username = document.querySelector("input").value;
-      alert(username);
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response.data);
     });
   });
